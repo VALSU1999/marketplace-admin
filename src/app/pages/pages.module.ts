31dfe07b3cd51f19d8673a7be61c636e1deb508c
victor <victoralexisluissalinas18@gmail.com>
2023-05-17 18:51:10 -0500

    Se creo la ruta y el login

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,39 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SharedModule } from '../shared/shared.module';
-import { HomeComponent } from './main-page/home/home.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { UsersComponent } from './main-page/users/users.component';
-import { CategoriesComponent } from './main-page/categories/categories.component';
-import { SubcategoriesComponent } from './main-page/subcategories/subcategories.component';
-import { StoresComponent } from './main-page/stores/stores.component';
-import { ProductsComponent } from './main-page/products/products.component';
-import { OrdersComponent } from './main-page/orders/orders.component';
-import { SalesComponent } from './main-page/sales/sales.component';
-import { DisputesComponent } from './main-page/disputes/disputes.component';
-import { MessagesComponent } from './main-page/messages/messages.component';
+
+import { AppRoutingModule} from '../app-routing.module';
+import { Error404Component } from './main-page/error404/error404.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    HomeComponent,
-    UsersComponent,
-    CategoriesComponent,
-    SubcategoriesComponent,
-    StoresComponent,
-    ProductsComponent,
-    OrdersComponent,
-    SalesComponent,
-    DisputesComponent,
-    MessagesComponent
+    Error404Component
   ],
   imports: [
     CommonModule,
     AppRoutingModule,
     SharedModule
-  ],
-  exports:[
-    MainPageComponent
   ]
 })
 export class PagesModule { }
